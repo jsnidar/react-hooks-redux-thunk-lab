123a468ab0928ec0c00e06e2143ff86e20c1da8a
Jeremiah <jsnidar@gmail.com>
2022-01-26 09:46:35 -0600

    Setup action creator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore } from "redux";
+import { Provider, applyMiddleware } from 'react-redux';
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import App from "./App";
+import catsReducer from '/features/cats/catsSlice.js';
+
+const store = createStore(catsReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>
+, document.getElementById("root"));
